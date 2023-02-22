38cfd704d14084f5a06261fb3727791fc91133e8
IpnDef <ipndef@gmail.com>
2023-02-22 12:12:31 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -126,23 +126,16 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
+      schoolName: "SMK Bani Rosa Insani",
       logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      subHeader: "SMU/SMK/STM in Ilmu Komputer/Teknologi Informasi | Indonesia",
+      duration: "Feb 2017 - Mei 2020",
+      desc: "Jurusan RPL Rekayasa Perangkat Lunak.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Sistem Komputer, Jaringan Komputer, Algoritma dan Pemrograman",
+        "Desain Grafis, Basis Data, Membuat Aplikasi Web dan Mobile",
+        "Desainer UI / UX – Web dan Mobile"
       ]
-    },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
     }
   ]
 };
